8503f8843921526ee6acd1cf0c048190cbcc45e2
Henry <henrycauan3222@gmail.com>
2025-10-26 23:07:13 -0300

    .

--- PostgreSQL + Supabase/_backend/router/userRouter.js
+++ PostgreSQL + Supabase/_backend/router/userRouter.js
@@ -11,10 +11,37 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
+  const { data, error } = await supabase.from("users").select("*").eq("id", id);
+});
+
+router.post("/", async (req, res) => {
+  const { name, age, email } = req.body;
   const { data, error } = await supabase
     .from("users")
-    .select("*")
+    .insert([{ name: name, age: age, email: email }]);
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+  res.status(200).json(data);
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nome, age, email } = req.body;
+  const { data, error } = await supabase
+    .from("usuarios")
+    .update({ nome, age, email })
     .eq("id", id);
+  if (error) return res.status(400).json({ error });
+  res.json(data);
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { error } = await supabase.from("usuarios").delete().eq("id", id);
+  if (error) return res.status(400).json({ error });
+  res.json({ message: "Usuário deletado" });
 });
 
 export default router;
--- PostgreSQL + Supabase/_backend/server.js
+++ PostgreSQL + Supabase/_backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from 'cors';
+import useRouter from  './router/userRouter'
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
+app.use('/users', useRouter);
 
 app.get('/', (req, res) => {
-  res.json();
+  res.staus(200).json({ message: 'Hello Word! Creator' });
 });
 
 app.listen(PORT, () => {
